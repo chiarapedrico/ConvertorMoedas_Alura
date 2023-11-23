6aa9a7f13669a23104c7eec674718815665d87a1
Chiara Pedrico <145162756+chiarapedrico@users.noreply.github.com>
2023-11-22 22:10:00 -0300

    Add files via upload

---
+++ Doramalandia Add/README.md
@@ -0,0 +1,4 @@
+# _Aluraflix2  - Projeto
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/chiarapedrico/pen/mdvLMzw](https://codepen.io/chiarapedrico/pen/mdvLMzw).
+
---
+++ Doramalandia Add/src/script.js
@@ -0,0 +1,8 @@
+function adicionarFilme(){
+  var filmeFav = document.getElementById('filme').value
+ var elementoListaFilmes = document.getElementById('listaFilmes')
+ elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFav + '>'
+  document.getElementById('filme').value = null;
+}
+  
+  
