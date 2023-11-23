b42d2ceef3223e60978576928d575022721f4e53
Chiara Pedrico <145162756+chiarapedrico@users.noreply.github.com>
2023-11-22 22:08:17 -0300

    Quarta aula

--- Doramalandia Add/src/script.js
+++
@@ -1,6 +0,0 @@
-function adicionarFilme(){
-  var filmeFav = document.getElementById('filme').value
- var elementoListaFilmes = document.getElementById('listaFilmes')
- elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + '<img src=' + filmeFav + '>'
-  document.getElementById('filme').value = null;
-}
